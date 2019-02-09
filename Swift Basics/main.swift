4492ea6d7cc5d9c46d3e75c9a3656c80f3096c3f
Boao Huang <bhgc9@mail.missouri.edu>
2019-02-08 18:43:48 -0600

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Boao Huang on 2/8/19.
+//  Copyright © 2019 Boao Huang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
